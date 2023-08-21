a1720539e10592f7a272c396e604629c30848613
DawnSheedy <dawn@dawnsheedy.com>
2023-08-20 21:47:43 -0700

    Initial Data Models

---
+++ src/schema/User.ts
@@ -0,0 +1,19 @@
+import { Schema, Types, model } from "mongoose";
+
+interface IUser {
+  email: string;
+  firstName: string;
+  lastName: string;
+  permissions: Types.ObjectId[];
+}
+
+const userSchema = new Schema<IUser>({
+  email: { type: String, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  permissions: [{ type: Types.ObjectId, ref: "Permission" }],
+});
+
+const User = model<IUser>('User', userSchema);
+
+export { User }
